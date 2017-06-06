f1c474088fec251e8db84a26d3f4587144a38746
Paul Smith <paulcsmith0218@gmail.com>
2017-06-06 14:38:58 -0400

    Add original asset paths

--- config/webpack/loaders/images.js
+++ config/webpack/loaders/images.js
@@ -1,12 +1,11 @@
 const { env, publicPath } = require('../configuration.js')
 
 module.exports = {
-  test: /images/i,
+  test: /\.(jpg|jpeg|png|gif|svg|eot|ttf|woff|woff2)$/i,
   use: [{
     loader: 'file-loader',
     options: {
       publicPath,
-      // outputPath: 'images/',
       name: env.NODE_ENV === 'production' ? '[folder]/[name]-[hash].[ext]' : '[folder]/[name].[ext]'
     }
   }]
